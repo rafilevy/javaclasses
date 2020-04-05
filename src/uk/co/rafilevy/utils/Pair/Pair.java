f0b79779b2a988376128f66206faa3958e20077f
Rafi Levy <rafi.levy@sky.com>
2020-04-05 14:17:38 +0100

    Added pair and triple docs

--- src/uk/co/rafilevy/utils/Pair/Pair.java
+++ src/uk/co/rafilevy/utils/Pair/Pair.java
@@ -2,16 +2,34 @@ package uk.co.rafilevy.utils.Pair;
 
 import java.util.Objects;
 
+/**
+ * An ordered pair storing two items of type {@code A}
+ * @param <A> the type of the items being stored
+ */
 public class Pair<A> {
     private A first;
     private A second;
 
+    /**
+     * Creates a pair with two given items
+     * @param first the first item in the pair
+     * @param second the second item in the pair
+     */
     public Pair(A first, A second) {
         this.first = first;
         this.second = second;
     }
 
+    /**
+     * Gets the first item from the pair
+     * @return the first item from the pair
+     */
     public A getFirst() { return first; }
+
+    /**
+     * Gets the second item from the pair
+     * @return the second item from the pair
+     */
     public A getSecond() { return second; }
 
     @Override
--- src/uk/co/rafilevy/utils/Pair/Trip.java
+++ src/uk/co/rafilevy/utils/Pair/Trip.java
@@ -2,19 +2,43 @@ package uk.co.rafilevy.utils.Pair;
 
 import java.util.Objects;
 
+/**
+ * An ordered triple storing three items of type {@code A}
+ * @param <A> the type of the stored items
+ */
 public class Trip<A> {
     private A first;
     private A second;
     private A third;
 
+    /**
+     * Creates a triple with three given items
+     * @param first the first item in the triple
+     * @param second the second item in the triple
+     * @param third the third item in the triple
+     */
     public Trip(A first, A second, A third) {
         this.first = first;
         this.second = second;
         this.third = third;
     }
 
+    /**
+     * Gets the first item in the triple
+     * @return the first item in the triple
+     */
     public A getFirst() { return first; }
+
+    /**
+     * Gets the second item in the triple
+     * @return the second item in the triple
+     */
     public A getSecond() { return second; }
+
+    /**
+     * Gets the third item in the triple
+     * @return the third item in the triple
+     */
     public A getThird() { return  third; }
 
     @Override
